db79a78b121066ef482576170f8950e42952e81b
wuzhiping <wuzhiping@users.noreply.github.com>
2023-03-27 18:04:05 +0800

    Add files via upload

---
+++ init.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+cd frappe-bench
+
+# Use containers instead of localhost
+bench set-mariadb-host mariadb
+bench set-redis-cache-host redis:6379
+bench set-redis-queue-host redis:6379
+bench set-redis-socketio-host redis:6379
+
+bench new-site lms.localhost \
+--force \
+--mariadb-root-password 123 \
+--admin-password admin \
+--no-mariadb-socket
+
+bench --site lms.localhost install-app lms
+bench --site lms.localhost set-config developer_mode 1
+bench --site lms.localhost clear-cache
+bench --site lms.localhost set-config mute_emails 1
+bench use lms.localhost
+
+bench start
